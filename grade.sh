a7f03c076b61c747b45d25a986d05b25adc3f514
zelbananyucsd <zelbanany@ucsd.edu>
2023-05-25 08:25:08 -0700

    done

--- grade.sh
+++ grade.sh
@@ -8,9 +8,46 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+if [ ! -f ListExamples.java ] 
+then 
+    echo "FileNotFound Error: ListExamples.java is missing"
+    exit 1 
+fi 
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cp ListExamples.java ../grading-area
+cp ../TestListExamples.java ../grading-area
+
+cd ../lib 
+
+cp hamcrest-core-1.3.jar ../grading-area
+cp junit-4.13.2.jar ../grading-area
+
+cd ../grading-area
+
+javac -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar *.java
+java -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
+if [ $? -ne 0 ] 
+then
+    echo "Did Not Compile"
+    exit 1 
+fi 
+
+
+output=$(java -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples)
+
+if [ $output == "FAILURES!!!"* ] 
+then 
+    echo "Did Not Pass Tests"
+    exit 1 
+fi
+
+tests=$(grep "Tests run: \K[0-9]+" <<< "$output")
+testsP=$(grep "Tests run: [0-9]+, Failures: \K[0-9]+" <<< "$output")
+
+score=$(awk -v testsP="$testsP" -v tests="$tests" 'BEGIN {print testsP / tests * 100}')
+
+echo "Grade: $score%"
--- grading-area/ListExamples.java
+++ grading-area/ListExamples.java
@@ -12,7 +12,7 @@ class ListExamples {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(s);
+        result.add(0, s)
       }
     }
     return result;
@@ -40,7 +40,7 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index2 += 1;
+      index1 += 1;
     }
     return result;
   }
